e40ef3dd5ff7024c0b11bfc8e8c2e95dce640368
Amy <amee_sk@yahoo.com>
2020-09-25 21:39:59 +0100

    Immplementing hit points

--- spec/attack_spec.rb
+++ spec/attack_spec.rb
@@ -6,4 +6,11 @@ feature ' Attacking 'do
       click_button 'Attack'
       expect(page).to have_content 'Amy attacked Anastasis'
   end
+  it 'reduces player 2s HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Anastasis: 60HP'
+    expect(page).to have_content 'Anastasis: 50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,31 @@
-require 'player'
+require '../lib/player.rb'
 
-describe 'Player' do
- it ''
+describe Player do
+  subject(:amy) {Player.new('Amy')}
+  subject(:anastasis) {Player.new('Anastasis')}
 
+  describe 'a name' do
+    it 'returns the name' do
+      expect(amy.name).to eq 'Amy'
+    end
+  end
+   
+  describe 'hit points' do
+    it 'returns the hit points' do
+      expect(amy.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
 
+  describe 'attack' do
+    it 'damages the player' do
+      expect(anastasis).to receive(:receive_damage)
+      amy.attack(anastasis)
+    end
+  end
+
+  describe 'receive_damage' do
+    it 'reduces the player hit points' do
+      expect { amy.receive_damage }.to change { amy.hit_points }.by(-10)
+    end
+  end
 end
